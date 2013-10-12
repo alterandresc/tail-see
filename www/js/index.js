72528b0d5ab117d606dc211c5126e7b5167448f9
Wilson Salamanca <wilson@wilson-Satellite-M645.(none)>
2013-10-12 09:28:27 -0500

    Marcador agregado

--- www/js/index.js
+++ www/js/index.js
@@ -62,12 +62,16 @@ var app = {
     },
     initializeMap: function()
     {
+	
       var mapOptions = {
 	zoom: 8,
 	center: new google.maps.LatLng(-34.397, 150.644),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
       }
       var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      var marker = new google.maps.Marker({
+		  position: center, map:map
+		  });
     },
     
 };
